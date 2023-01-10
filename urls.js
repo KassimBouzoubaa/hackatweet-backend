acc291e88bc49208c857928c39c452c90067115e
Kassim <kassimbouzoubaa05@gmail.com>
2023-01-10 19:26:12 +0100

    vercel

---
+++ urls.js
@@ -0,0 +1,7 @@
+// Insert the backend URL given by Vercel in this variable
+const BACKEND_URL = 'https://hackatweet-backend-blue.vercel.app';
+
+// Insert the frontend URL given by Vercel in this variable
+const FRONTEND_URL = 'https://mymovies-frontend.vercel.app';
+
+module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
